b62c44f37f1255ba450774eb28831e077064cb4f
konstantinos.zartaloudis <konstantinos.zartaloudis@instashop.ae>
2023-07-20 12:42:05 +0300

    Bearer token thanks God Gpt4

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ const express = require('express');
 const axios = require('axios');
 const { Parser } = require('json2csv');
 const js2xmlparser = require('js2xmlparser');
-const fs = require('fs').promises; // Node.js file system module for handling files
+const fs = require('fs').promises;
 const PORT = 3000;
 const app = express();
 
-app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send(`
         <h1>JSON to CSV/XML Converter by Zartas </h1>
-        <p>Enter a JSON URL and select a format to convert the JSON to.</p>
+        <p>Enter a JSON URL, bearer token (if required), and select a format to convert the JSON to.</p>
         <p style="color: red;">Αυτο το εργαλειο μπορει να χρησιμοποιηθει μονο απο ενα χρηστη την φορα , θα πρεπει να ρυθμιστει ο web server επισης να περιμενει to processing</p>
         <form action="/convert" method="POST">
             <label for="jsonUrl">JSON URL:</label><br>
             <input type="text" id="jsonUrl" name="jsonUrl" style="width: 100%;"><br>
+            <label for="bearerToken">Bearer Token (optional):</label><br>
+            <input type="text" id="bearerToken" name="bearerToken" style="width: 100%;"><br>
             <input type="radio" id="csv" name="format" value="csv">
             <label for="csv">CSV</label><br>
             <input type="radio" id="xml" name="format" value="xml">
@@ -27,7 +29,12 @@ app.get('/', (req, res) => {
 
 app.post('/convert', async (req, res) => {
     try {
-        const jsonData = await axios.get(req.body.jsonUrl);
+        const config = {};
+        if (req.body.bearerToken) {
+            config.headers = { Authorization: `Bearer ${req.body.bearerToken}` };
+        }
+
+        const jsonData = await axios.get(req.body.jsonUrl, config);
         let output;
         let filename;
 
@@ -40,9 +47,9 @@ app.post('/convert', async (req, res) => {
             filename = 'output.xml';
         }
 
-        await fs.writeFile(filename, output); // write the output to a file
+        await fs.writeFile(filename, output);
 
-        res.download(filename); // serve the file as a download
+        res.download(filename);
     } catch (error) {
         console.error(error);
         res.status(500).send('An error occurred.');
